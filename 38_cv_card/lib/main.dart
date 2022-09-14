fb051d1b56e90935cc94f27e1fe55c7e117ae9ee
pablobagdal <pablobagdal@gmail.com>
2022-09-15 02:01:53 +0300

    separated code to diff stl widgets

--- 38_cv_card/lib/main.dart
+++ 38_cv_card/lib/main.dart
@@ -37,133 +37,212 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.all(
             10.0,
           ),
-          child: AspectRatio(
-            aspectRatio: 1.5,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  26.0,
-                ),
-              ),
-              elevation: 18.0,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(
-                  26.0,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Flexible(
-                      flex: 3,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.all(
-                              12.0,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Павел Багдалов",
-                                  style: TextStyle(
-                                    fontSize: 28,
-                                  ),
-                                ),
-                                Text(
-                                  "Работаю на себя",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const Spacer(
-                                flex: 1,
-                              ),
-                              Flexible(
-                                child: IconButton(
-                                  iconSize: 32,
-                                  icon: const Icon(
-                                    CVIcons.telegram,
-                                  ),
-                                  onPressed: () => launchUrl(
-                                    Uri.parse(
-                                      Links.telegram,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Flexible(
-                                child: IconButton(
-                                  iconSize: 32,
-                                  icon: const Icon(
-                                    CVIcons.github,
-                                  ),
-                                  onPressed: () => launchUrl(
-                                    Uri.parse(
-                                      Links.github,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Flexible(
-                                child: IconButton(
-                                  iconSize: 32,
-                                  icon: const Icon(
-                                    CVIcons.email,
-                                  ),
-                                  onPressed: () {
-                                    // launchUrl(
-                                    //   Uri.parse(
-                                    //     Links.email,
-                                    //   ),
-                                    // );
-                                    Clipboard.setData(
-                                      const ClipboardData(
-                                        text: Links.email,
-                                      ),
-                                    );
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content: Text(
-                                          "Скопировано",
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                              const Spacer(
-                                flex: 1,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      flex: 2,
-                      child: Image.asset(
-                        _tgAvatar,
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                  ],
-                ),
+          child: CVCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class CVCard extends StatelessWidget {
+  const CVCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CVCardContainer(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Flexible(
+            flex: 3,
+            child: InfoWidget(),
+          ),
+          Flexible(
+            flex: 2,
+            child: AvatarWidget(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CVCardContainer extends StatelessWidget {
+  static const _borderRadius = 16.0;
+  final Widget child;
+
+  const CVCardContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.5,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            26.0,
+          ),
+        ),
+        elevation: 18.0,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(
+            26.0,
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class InfoWidget extends StatelessWidget {
+  const InfoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(
+            12.0,
+          ),
+          child: IdentifyWidget(),
+        ),
+        LinksWidget(),
+      ],
+    );
+  }
+}
+
+class AvatarWidget extends StatelessWidget {
+  const AvatarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      _tgAvatar,
+      fit: BoxFit.fitHeight,
+      frameBuilder: (_, child, frame, __) => AnimatedOpacity(
+        opacity: frame == null ? 0 : 1.0,
+        duration: const Duration(
+          milliseconds: 1500,
+        ),
+        child: frame == null ? Container() : child,
+      ),
+    );
+  }
+}
+
+class IdentifyWidget extends StatelessWidget {
+  const IdentifyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Павел Багдалов",
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        const Text(
+          "Работаю на себя",
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LinksWidget extends StatelessWidget {
+  const LinksWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Spacer(
+          flex: 1,
+        ),
+        Flexible(
+          child: LinkIcon(
+            icon: CVIcons.telegram,
+            onPressed: () => launchUrl(
+              Uri.parse(Links.telegram),
+            ),
+          ),
+        ),
+        Flexible(
+          child: IconButton(
+            iconSize: 32,
+            icon: const Icon(
+              CVIcons.github,
+            ),
+            onPressed: () => launchUrl(
+              Uri.parse(
+                Links.github,
               ),
             ),
           ),
         ),
-      ),
+        Flexible(
+          child: IconButton(
+            iconSize: 32,
+            icon: const Icon(
+              CVIcons.email,
+            ),
+            onPressed: () {
+              Clipboard.setData(
+                const ClipboardData(
+                  text: Links.email,
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text(
+                    "Скопировано",
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        const Spacer(
+          flex: 1,
+        ),
+      ],
+    );
+  }
+}
+
+class LinkIcon extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const LinkIcon({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      iconSize: 32,
+      icon: Icon(icon),
+      onPressed: onPressed,
     );
   }
 }
